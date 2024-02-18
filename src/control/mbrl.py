18dd06a09e373f205b23681ee8988f4efdb3a880
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-02-18 15:14:22 -0800

    Added method for updating dynamics model

--- src/control/mbrl.py
+++ src/control/mbrl.py
@@ -1,5 +1,7 @@
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from core import DynamicsModel
 
 
@@ -8,7 +10,7 @@ class MBRLLearner:
     A class for training a model-based reinforcement learning agent.
     """
 
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, state_dim, action_dim, lr=1e-3):
         """
         Parameters
         ----------
@@ -16,10 +18,19 @@ class MBRLLearner:
             Dimension of the state space.
         action_dim : int
             Dimension of the action space.
+        lr : float
+            Learning rate for dynamics model.
         """
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print(f"Using device: {device}")
-        self.model = DynamicsModel(state_dim, action_dim).to(device)
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.lr = lr
+        self.replay_buffer = []
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(f"Using device: {self.device}")
+        self.model = DynamicsModel(self.state_dim, self.action_dim).to(self.device)
+        self.loss = nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
 
     def train(self, env, num_episodes, episode_len):
         """
@@ -31,15 +42,28 @@ class MBRLLearner:
         episode_len : int
             The length of each episode.
         """
-        replay_buffer = []
 
         for ep in range(num_episodes):
-            # Train model on replay buffer
+            self.update_dynamics()
             o, _ = env.reset()
             for t in range(episode_len):
-                action = 0
+                action = 0 # MPC goes here
                 next_o, reward, terminated, truncated, _ = env.step(action)
                 if terminated or truncated:
                     break
-                replay_buffer.append((o, action, next_o))
+                self.replay_buffer.append((o, action, next_o))
+                o = next_o
             env.close()
+
+    def update_dynamics(self):
+        """
+        Update the dynamics model using sampled (s,a,s') triplets stored in replay_buffer.
+        """
+        input = []  # Figure out how to get input and target.
+        target = []
+
+        self.optimizer.zero_grad()
+        output = self.model(input)
+        loss = self.loss(output, target)
+        loss.backward()
+        self.optimizer.step()
