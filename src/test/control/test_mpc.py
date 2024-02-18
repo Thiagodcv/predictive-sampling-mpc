e983ecc4b7dd4b1f5c3c64a0b8726662e51d7f66
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-02-17 22:38:40 -0800

    Added testing suite

---
+++ src/test/control/test_mpc.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from control.mpc import MPC
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TestMPC(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def nn_random_shooting(self):
+        pass
