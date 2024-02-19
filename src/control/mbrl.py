86c46d28f7d998fc1f6ea5e899ee152312893f4e
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-02-18 18:02:41 -0800

    finished filling in MBRL. Time for lots of debugging.

--- src/control/mbrl.py
+++ src/control/mbrl.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from core import DynamicsModel
+from mpc import MPC
 
 
 class MBRLLearner:
@@ -10,7 +11,7 @@ class MBRLLearner:
     A class for training a model-based reinforcement learning agent.
     """
 
-    def __init__(self, state_dim, action_dim, lr=1e-3):
+    def __init__(self, state_dim, action_dim, env, num_episodes, episode_len, lr=1e-3, batch_size=16):
         """
         Parameters
         ----------
@@ -18,49 +19,74 @@ class MBRLLearner:
             Dimension of the state space.
         action_dim : int
             Dimension of the action space.
+        env : gym.Env
+        num_episodes : int
+            Number of episodes to train for.
+        episode_len : int
+            The length of each episode.
         lr : float
             Learning rate for dynamics model.
+        batch_size : int
+            Batch size for dynamics model training.
         """
+        # RL Training Parameters
         self.state_dim = state_dim
         self.action_dim = action_dim
-        self.lr = lr
+        self.env = env
+        self.num_episodes = num_episodes
+        self.episode_len = episode_len
         self.replay_buffer = []
 
+        # Dynamics Model Trainings Parameters
+        self.lr = lr
+        self.batch_size = batch_size
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print(f"Using device: {self.device}")
         self.model = DynamicsModel(self.state_dim, self.action_dim).to(self.device)
         self.loss = nn.MSELoss()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
 
-    def train(self, env, num_episodes, episode_len):
+        # MPC Parameters
+        self.policy = MPC(self.model)
+        self.num_traj = 50
+        self.gamma = 0.95
+        self.horizon = 10
+
+        def reward(state, action):
+            return 0
+        self.reward = reward
+
+    def train(self):
         """
-        Parameters
-        ----------
-        env : gym.Env
-        num_episodes : int
-            Number of episodes to train for.
-        episode_len : int
-            The length of each episode.
+        Train the MBRL agent.
         """
 
-        for ep in range(num_episodes):
+        for ep in range(self.num_episodes):
             self.update_dynamics()
-            o, _ = env.reset()
-            for t in range(episode_len):
-                action = 0 # MPC goes here
-                next_o, reward, terminated, truncated, _ = env.step(action)
+            o, _ = self.env.reset()
+            for t in range(self.episode_len):
+                action = self.policy.random_shooting(o, [self.replay_buffer[-i][1] for i in range(3)],
+                                                     self.num_traj, self.gamma, self.horizon, self.reward)
+                next_o, reward, terminated, truncated, _ = self.env.step(action)
                 if terminated or truncated:
                     break
                 self.replay_buffer.append((o, action, next_o))
                 o = next_o
-            env.close()
+            self.env.close()
 
     def update_dynamics(self):
         """
         Update the dynamics model using sampled (s,a,s') triplets stored in replay_buffer.
         """
-        input = []  # Figure out how to get input and target.
+        input = []
         target = []
+
+        batch_idx = torch.randint(len(self.replay_buffer), size=(self.batch_size,)).item()
+        batch = self.replay_buffer[batch_idx]
+        for i in range(len(batch)):
+            input[i].append(np.concatenate((batch[i][0], batch[i][1])))
+            s_diff = batch[i][2] - batch[i][0]
+            target.append(s_diff)
 
         self.optimizer.zero_grad()
         output = self.model(input)
