7bb4caccfc27113580e5b9a376c31f65f5c65672
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-02-19 22:24:35 -0800

    Made it so that normalization occures when sampling from replay buffer

--- src/control/replay_buffer.py
+++ src/control/replay_buffer.py
@@ -4,8 +4,7 @@ import collections
 import numpy as np
 import torch
 
-# n_ = normalized, d_ = difference
-Transition = collections.namedtuple('Transition', ('n_state', 'n_action', 'd_n_state'))
+Transition = collections.namedtuple('Transition', ('state', 'action', 'next_state'))
 device = 'cpu'
 
 
@@ -23,46 +22,51 @@ class ReplayBuffer:
         Push (s, a, s') tuple to replay memory where 'state', 'action', and 'next_state' are not yet normalized.
         """
         # Update means and variances
-        self.state_mean = (self.__len__()*self.state_mean + state) / (self.__len__() + 1)
-        self.action_mean = (self.__len__()*self.action_mean + action) / (self.__len__() + 1)
-        self.state_var = ((self.__len__() - 1)*self.state_var +
-                          np.square(state - self.state_mean))/(self.__len__() - 1)
-        self.action_var = ((self.__len__() - 1)*self.action_var +
-                           np.square(action - self.action_mean))/(self.__len__() - 1)
-
-        # Normalize data
-        n_state = np.diagflat(np.reciprocal(self.state_var)) @ (state - self.state_mean)
-        n_action = np.diagflat(np.reciprocal(self.action_var)) @ (action - self.action_mean)
-        n_next_state = np.diagflat(np.reciprocal(self.state_var)) @ (next_state - self.state_mean)
-        d_n_state = n_next_state - n_state
-
-        # Add zero-mean gaussian noise to tuple variables
-        n_state += self.sample_state_gaussian()
-        n_action += self.sample_action_gaussian()
-        n_next_state += self.sample_state_gaussian()
+        if self.__len__() > 1:
+            self.state_mean = (self.__len__()*self.state_mean + state) / (self.__len__() + 1)
+            self.action_mean = (self.__len__()*self.action_mean + action) / (self.__len__() + 1)
+            self.state_var = ((self.__len__() - 1)*self.state_var +
+                              np.square(state - self.state_mean))/(self.__len__() + 1 - 1)
+            self.action_var = ((self.__len__() - 1)*self.action_var +
+                               np.square(action - self.action_mean))/(self.__len__() + 1 - 1)
 
         # Push normalized data into replay buffer
-        transition = Transition(n_state, n_action, d_n_state)
+        transition = Transition(state, action, next_state)
         self.data.append(transition)
-
-    def sample_state_gaussian(self):
-        return np.random.multivariate_normal(mean=np.zeros(len(self.state_dim)),
-                                             cov=0.01*np.eye(len(self.state_dim)),
-                                             size=len(self.state_dim))
-
-    def sample_action_gaussian(self):
-        return np.random.multivariate_normal(mean=np.zeros(len(self.action_dim)),
-                                             cov=0.01*np.eye(len(self.action_dim)),
-                                             size=len(self.action_dim))
 
     def sample(self, batch_size):
         transitions = random.sample(self.data, batch_size)
-
         batch = Transition(*zip(*transitions))
-        n_state = torch.tensor(batch.n_state, device=device, dtype=torch.float32)
-        n_action = torch.tensor(batch.n_action, device=device, dtype=torch.float32)
-        d_n_state = torch.tensor(batch.d_n_state, device=device, dtype=torch.float32)
+
+        state = np.array(batch.state)
+        action = np.array(batch.action)
+        next_state = np.array(batch.next_state)
+
+        # Normalize data
+        n_state = (state - self.state_mean) @ np.diagflat(np.reciprocal(self.state_var))
+        n_action = (action - self.action_mean) @ np.diagflat(np.reciprocal(self.action_var))
+        n_next_state = (next_state - self.state_mean) @ np.diagflat(np.reciprocal(self.state_var))
+        d_n_state = n_next_state - n_state
+
+        # Add zero-mean gaussian noise to tuple variables
+        n_state += self.sample_state_gaussian(batch_size)
+        n_action += self.sample_action_gaussian(batch_size)
+        d_n_state += self.sample_state_gaussian(batch_size)
+
+        n_state = torch.tensor(n_state, device=device, dtype=torch.float32)
+        n_action = torch.tensor(n_action, device=device, dtype=torch.float32)
+        d_n_state = torch.tensor(d_n_state, device=device, dtype=torch.float32)
         return n_state, n_action, d_n_state
+
+    def sample_state_gaussian(self, size):
+        return np.random.multivariate_normal(mean=np.zeros(self.state_dim),
+                                             cov=0.01*np.eye(self.state_dim),
+                                             size=size)
+
+    def sample_action_gaussian(self, size):
+        return np.random.multivariate_normal(mean=np.zeros(self.action_dim),
+                                             cov=0.01*np.eye(self.action_dim),
+                                             size=size)
 
     def __len__(self):
         return len(self.data)
