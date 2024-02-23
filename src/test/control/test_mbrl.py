34d44b45db8cbf812474ea9142ee35ad1756d16d
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-02-23 11:31:56 -0800

    Improved MPC hyperparameters

--- src/test/control/test_mbrl.py
+++ src/test/control/test_mbrl.py
@@ -69,5 +69,5 @@ class TestMBRL(TestCase):
         learner = MBRLLearner(state_dim=state_dim, action_dim=action_dim, env=env,
                               num_episodes=num_episodes, episode_len=episode_len, reward=reward,
                               terminate=terminate, batch_size=batch_size, train_buffer_len=train_buffer_len,
-                              save_name="testv7")
+                              save_name="testv8")
         learner.train()
