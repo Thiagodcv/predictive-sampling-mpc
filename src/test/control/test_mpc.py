9a68d3da6511da1ba00735721083a00ac494378f
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-02-23 11:47:31 -0800

    Added a test to see if MPC handles termination correctly

--- src/test/control/test_mpc.py
+++ src/test/control/test_mpc.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
 from src.control.dynamics import DynamicsModel
+from src.constants import MODELS_PATH
 from control.mpc import MPC
 import gymnasium as gym
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import os
 
 
 class TestMPC(TestCase):
@@ -16,54 +16,53 @@ class TestMPC(TestCase):
     def tearDown(self):
         pass
 
-    def test_nn_random_shooting(self):
-        """
-        Test to see if can run an example without crashing.
-        """
-        PATH = "C:/Users/thiag/Git/random-shooting-mpc/models/good_model.pt"
-        state_dim = 4
-        action_dim = 1
-
-        # input params
-        num_traj = 1000
-        gamma = 0.99
-        horizon = 10
-
-        env = gym.make("InvertedPendulum-v4")  # ,render_mode="human")
-        model = DynamicsModel(state_dim, action_dim)
-        model.load_state_dict(torch.load(PATH))
-
-        def reward(state, action):
-            return 1
-
-        def terminate(state, action, t):
-            # If episode is at t>=1000, terminate episode
-            if t >= 1000:
-                return True
-            # If absolute value of vertical angle between pole and cart is greater than 0.2,
-            # terminate episode
-            elif state[1] > 0.2 or state[1] < -0.2:
-                return True
-            else:
-                return False
-
-        mpc = MPC(model, num_traj, gamma, horizon, reward, terminate)
-
-        state, _ = env.reset()
-        for t in range(100):
-            print(t)
-            action = mpc.random_shooting(state)
-            next_state, reward, terminated, truncated, _ = env.step(action)
-            if terminated or truncated:
-                break
-            state = next_state
+    # def test_nn_random_shooting(self):
+    #     """
+    #     Test to see if can run an example without crashing.
+    #     """
+    #     PATH = "C:/Users/thiag/Git/random-shooting-mpc/models/good_model.pt"
+    #     state_dim = 4
+    #     action_dim = 1
+    #
+    #     # input params
+    #     num_traj = 1000
+    #     gamma = 0.99
+    #     horizon = 10
+    #
+    #     env = gym.make("InvertedPendulum-v4")  # ,render_mode="human")
+    #     model = DynamicsModel(state_dim, action_dim)
+    #     model.load_state_dict(torch.load(PATH))
+    #
+    #     def reward(state, action):
+    #         return 1
+    #
+    #     def terminate(state, action, t):
+    #         # If episode is at t>=1000, terminate episode
+    #         if t >= 1000:
+    #             return True
+    #         # If absolute value of vertical angle between pole and cart is greater than 0.2,
+    #         # terminate episode
+    #         elif state[1] > 0.2 or state[1] < -0.2:
+    #             return True
+    #         else:
+    #             return False
+    #
+    #     mpc = MPC(model, num_traj, gamma, horizon, reward, terminate)
+    #
+    #     state, _ = env.reset()
+    #     for t in range(100):
+    #         print(t)
+    #         action = mpc.random_shooting(state)
+    #         next_state, reward, terminated, truncated, _ = env.step(action)
+    #         if terminated or truncated:
+    #             break
+    #         state = next_state
 
     def test_find_discrete_optimal_action(self):
         """
         Test to see if random shooting mpc can find optimal set of actions.
         Note that the action space = {0, 1}.
         """
-        PATH = "C:/Users/thiag/Git/random-shooting-mpc/models/good_model.pt"
         # input params
         num_traj = 100
         gamma = 1e-5
@@ -73,7 +72,7 @@ class TestMPC(TestCase):
         state_dim = 4
         action_dim = 1
         model = DynamicsModel(state_dim, action_dim)
-        model.load_state_dict(torch.load(PATH))
+        model.load_state_dict(torch.load(os.path.join(MODELS_PATH, "testv8.pt")))
 
         # Defining the reward
         optimal_action = 1
@@ -87,4 +86,33 @@ class TestMPC(TestCase):
         for i in range(100):
             print("action: ", mpc.random_shooting(state_dummy))
 
+    def test_termination_function(self):
+        """
+        Test to see if random shooting mpc avoid outputting "1" if environment
+        terminates when action=1 is taken.
+        """
+        # input params
+        num_traj = 100
+        gamma = 1e-5
+        horizon = 20
 
+        # Defining the model just to get the code to run
+        state_dim = 4
+        action_dim = 1
+        model = DynamicsModel(state_dim, action_dim)
+        model.load_state_dict(torch.load(os.path.join(MODELS_PATH, "testv8.pt")))
+
+        def reward(state, action):
+            return 1
+
+        def terminate(state, action, t):
+            if action == 1:
+                return True
+            else:
+                return False
+
+        mpc = MPC(model, num_traj, gamma, horizon, reward, terminate)
+
+        state_dummy = np.zeros(state_dim)
+        for i in range(100):
+            print("action: ", mpc.random_shooting(state_dummy))
