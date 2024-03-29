a3f59f320f036b5f69f1e1e91d5934160969b07b
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-03-28 21:55:46 -0700

    Uncommented multithreading

--- src/experiments/pendulum.py
+++ src/experiments/pendulum.py
@@ -40,11 +40,11 @@ def pendulum():
     horizon = 15
 
     # Ray stuff
-    # num_workers = multiprocessing.cpu_count()
-    # print("Number of workers: ", num_workers)
-    # ray.init(num_cpus=num_workers)
+    num_workers = multiprocessing.cpu_count()
+    print("Number of workers: ", num_workers)
+    ray.init(num_cpus=num_workers)
 
-    mpc = MPC(model, num_traj, gamma, horizon, reward, multithreading=False)
+    mpc = MPC(model, num_traj, gamma, horizon, reward, multithreading=True)
 
     start_time = time.time()
     MBRLLearner.static_eval_model(env, episode_len, mpc, gamma)
