a623a8214d4987efe1d0d779bc424a0c99f36672
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-03-29 23:45:32 -0700

    Now working with 256 model

--- src/experiments/pendulum.py
+++ src/experiments/pendulum.py
@@ -33,7 +33,7 @@ def pendulum():
     env = gym.make("Pendulum-v1", render_mode="human")
 
     model = DynamicsModel(state_dim, action_dim, normalize=True)
-    model.load_state_dict(torch.load(os.path.join(MODELS_PATH, "pend_demo.pt")))
+    model.load_state_dict(torch.load(os.path.join(MODELS_PATH, "pend_demo_256.pt")))
 
     num_traj = 200
     gamma = 0.95
